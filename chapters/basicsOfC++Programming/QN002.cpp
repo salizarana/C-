6498263d2fe254aaa4bbc177a4168a44a075848d
salizarana <salizarana@gmail.com>
2021-03-27 19:26:40 +0545

    Add QN003.

---
+++ chapters/basicsOfC++Programming/QN002.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+
+int main(){
+    //This is single line comment
+    /*This is multi-lined comment.
+      This line will be ignored.*/
+
+    return 0;
+}
---
+++ chapters/basicsOfC++Programming/QN003.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int x, y, sum;
+
+    cout << "Enter any numbers:\n";
+    cin >> x >> y;
+
+    sum = x + y;
+    cout << "Sum of the numbers: " << sum << endl;
+
+    return 0;
+}
