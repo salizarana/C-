d92f015be760cc3ab6a83162a588eb379b627eda
salizarana <salizarana@gmail.com>
2021-05-05 13:47:58 +0545

    Add QN003.

---
+++ chapters/classes-and-objects/QN003.cpp
@@ -0,0 +1,33 @@
+/**
+ * Write a program defining member function outside the class.
+ */
+
+#include<iostream>
+using namespace std;
+
+class Sum{
+    public:
+    int addition(int x, int y);
+};
+
+/* Defining member function outside the class
+using scope resolution operator
+*/
+int Sum::addition(int x, int y){
+    return x + y;
+}
+
+int main() {
+    Sum obj;
+    int x, y, sum;
+
+    cout << "Enter two numbers: ";
+    cin >> x >> y;
+
+    sum = obj.addition(x, y);
+
+    cout << "Sum of two numbers: " << sum << "\n";
+
+    return 0;
+}
+
