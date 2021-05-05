d1fa9cb49bf5ead7e3d79ff962f54abd6eb90dda
salizarana <salizarana@gmail.com>
2021-05-05 13:26:00 +0545

    Example of class and object.

---
+++ chapters/classes-and-objects/QN002.cpp
@@ -0,0 +1,39 @@
+/**
+ * Write a program to illustrate an example of object creation when a 
+ * class is defined.
+ */
+
+#include<iostream>
+using namespace std;
+
+class Room {
+    public:
+    double length;
+    double breadth;
+    double height;
+
+    double calculateArea() {
+        return length * breadth;
+    }
+
+    double calculateVolume() {
+        return length * breadth * height;
+    }
+};
+
+int main() {
+    // create object of Room class
+    Room room1;
+
+    // assign values to data members
+    room1.length = 42.5;
+    room1.breadth = 30.8;
+    room1.height = 19.2;
+
+    // calculate and display the area and volume of the room
+    cout << "Area of Room =  " << room1.calculateArea() << endl;
+    cout << "Volume of Room =  " << room1.calculateVolume() << endl;
+
+    return 0;
+}
+
