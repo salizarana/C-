8fe678b7e85e93264b6c04247cde83dee6e623b1
salizarana <salizarana@gmail.com>
2021-05-13 20:00:24 +0545

    Format code

--- chapters/classes-and-objects/QN010.cpp
+++ chapters/classes-and-objects/QN010.cpp
@@ -5,30 +5,28 @@
 #include<iostream>
 using namespace std;
 
-class Point{
+class Point {
     private:
-    double x, y;
+        double x, y;
 
     public:
-    Point(double x1, double y1){
+        Point(double x1, double y1) {
         x = x1;
         y = y1;
     }
-    Point(const Point &p1){
+    Point(const Point &p1) {
         x = p1.x;
         y = p1.y;
     }
-
-    int getX(){
+    int getX() {
         return x;
     }
-
-    int getY(){
+    int getY() {
         return y;
     }
 };
 
-int main(){
+int main() {
     Point p1(10,15); // Normal constructor is called here
     Point p2 = p1; // Copy constructor is called here
 
--- chapters/classes-and-objects/QN011.cpp
+++ chapters/classes-and-objects/QN011.cpp
@@ -8,28 +8,26 @@ using namespace std;
 
 class Point{
     private:
-    double x, y;
+        double x, y;
 
     public:
-    Point(double x1 = 10, double y1 = 15){ //  default arguments
-        x = x1;
-        y = y1;
-    }
-    Point(const Point &p1){
-        x = p1.x;
-        y = p1.y;
-    }
-
-    int getX(){
-        return x;
-    }
-
-    int getY(){
-        return y;
-    }
+        Point(double x1 = 10, double y1 = 15) { //  default arguments
+            x = x1;
+            y = y1;
+        }
+        Point(const Point &p1) {
+            x = p1.x;
+            y = p1.y;
+        }
+        int getX() {
+            return x;
+        }
+        int getY() {
+            return y;
+        }
 };
 
-int main(){
+int main() {
     Point p1;
     Point p2 = p1;
 
