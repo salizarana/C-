689bb363eab656a91b61208fc2126c52fce09f18
salizarana <salizarana@gmail.com>
2021-05-15 19:06:24 +0545

    Static members

--- chapters/classes-and-objects/QN007.cpp
+++ chapters/classes-and-objects/QN007.cpp
@@ -1,3 +1,32 @@
 /**
- *
+ * Create a static variable count, static function in a class; create an object,
+ * call increment function and display count value twice in order.
  */
+
+#include <iostream>
+using namespace std;
+
+class Count {
+    private:
+        static int count;
+
+    public:
+        void setNum() {
+            count++;
+        }
+        static void putCount() {
+            cout << "Count:" << count << endl; // static function accessing static member
+        }
+};
+
+int Count::count;
+
+int main() {
+    Count c1, c2;
+
+    c1.setNum();
+    c2.setNum();
+    Count::putCount();
+
+    return 0;
+}
