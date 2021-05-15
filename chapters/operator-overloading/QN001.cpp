f36b5cf309c758a11bd91daf6a71a25505652856
salizarana <salizarana@gmail.com>
2021-05-15 20:09:08 +0545

    Unary operator overloading

---
+++ chapters/operator-overloading/QN001.cpp
@@ -0,0 +1,43 @@
+/**
+ * Write a program to show an example of unary operator overloading.
+ */
+
+#include<iostream>
+using namespace std;
+
+class Count {
+    private:
+        int value;
+
+    public:
+        // Constructor to initializa count to 5
+        Count() : value(5) {}
+
+        // Overload ++ when used as prefix
+        void operator ++ (){
+            ++value;
+        }
+
+        // Overload ++ when used as postfix
+        void operator ++ (int) {
+            value++;
+        }
+
+        void display() {
+            cout << "Count: " << value << endl;
+        }
+};
+
+int main() {
+    Count count;
+
+    // Call the "void operator ++ (int)" function
+    count++;
+    count.display();
+
+    // Call the "void operator ++ ()" function
+    ++count;
+    count.display();
+
+    return 0;
+}
